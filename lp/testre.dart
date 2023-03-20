7a16331ce0f443df1a826758fd629b30a9faac45
MARIA-LUIZ4 <127783564+MARIA-LUIZ4@users.noreply.github.com>
2023-03-20 01:31:30 -0400

    Delete testre.dart

--- lp/testre.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-void main(){
-  print("Olá eu sou o jogador  Nº 1, atual dono do jogo virtual OASIS, como deseja ser chamado? ");
-
-  String? nome = stdin.readLineSync();
-
-  print("Bem vindo ao OASIS, a melhor realidade virtual do mundo, $nome!");
-}
